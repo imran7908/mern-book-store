57de69866d152829e3f197caf133dc4a83ee040e
Imran <imran1510043@gmail.com>
2022-08-30 19:48:19 +0600

    frontend - AddBook component done

--- frontend/src/components/AddBook.js
+++ frontend/src/components/AddBook.js
@@ -1,9 +1,126 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  FormLabel,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const AddBook = () => {
+const AddBook = ({ URL }) => {
+  const history = useNavigate();
+
+  const [inputs, setInputs] = useState({
+    name: "",
+    author: "",
+    description: "",
+    price: "",
+    image: "",
+  });
+
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const sendRequest = async () => {
+    await axios
+      .post(`${URL}/books`, {
+        name: String(inputs.name),
+        author: String(inputs.author),
+        description: String(inputs.description),
+        price: Number(inputs.price),
+        available: Boolean(checked),
+        image: String(inputs.image),
+      })
+      .then((res) => res.data);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendRequest().then(() => history("/books"));
+  };
+
   return (
-    <div>AddBook</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        maxWidth="700px"
+        justifyContent="center"
+        alignSelf="center"
+        marginLeft="auto"
+        marginRight="auto"
+        marginTop="10px"
+      >
+        <FormLabel>Name</FormLabel>
+        <TextField
+          value={inputs.name}
+          onChange={handleChange}
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          name="name"
+        />
+        <FormLabel>Author</FormLabel>
+        <TextField
+          value={inputs.author}
+          onChange={handleChange}
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          name="author"
+        />
+        <FormLabel>Description</FormLabel>
+        <TextField
+          value={inputs.description}
+          onChange={handleChange}
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          name="description"
+        />
+        <FormLabel>Price</FormLabel>
+        <TextField
+          value={inputs.price}
+          onChange={handleChange}
+          type="number"
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          name="price"
+        />
+        <FormLabel>Image</FormLabel>
+        <TextField
+          value={inputs.image}
+          onChange={handleChange}
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          name="image"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={checked}
+              onChange={(e) => setChecked(!checked)}
+            />
+          }
+          label="Available"
+        />
+        <Button type="submit" variant="contained">
+          Add Book
+        </Button>
+      </Box>
+    </form>
+  );
+};
 
-export default AddBook
+export default AddBook;
