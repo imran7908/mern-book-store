be96271d5b6d5dd8a4776b3b0414595a8fc831cd
Imran <imran1510043@gmail.com>
2022-08-30 22:32:45 +0600

    frontend - About component done

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import { fontFamily } from "@mui/system";
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography sx={{ fontFamily: "fantasy" }} variant="h2">
+          This is a CRUD app about Book Store
+        </Typography>
+        <Typography variant="h3">Using MERN stack</Typography>
+      </Box>
+    </div>
+  );
+};
 
-export default About
+export default About;
